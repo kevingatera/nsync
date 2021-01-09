0addd9d9af36d10c4ea285442427a7fc16f6cc45
kevingatera <kgate066@uottawa.ca>
2021-01-09 14:12:42 -0500

    Format all songs's time played from milli to secs

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -7,6 +7,14 @@ export class AppService {
   private songsPlayed: SongPlay[];
 
   constructor() {
+    // var result: SongPlay[];
+
+    // Format all songs's time played from milli to secs
+    for (var i = 0; i < songPlays.length; i++) {
+      songPlays[i].msPlayed = Math.floor((songPlays[i].msPlayed / 1000) % 60);
+    }
+    // this.songsPlayed = results;
+
     this.songsPlayed = songPlays;
   }
 
